23c615faf02dad62abb25461f8c5a48309d046c6
Tanabe Yumi <alleyoop58dank@gmail.com>
2021-10-27 11:45:22 +0900

    Correct for loop lange.

--- kadai4-1.rb
+++ kadai4-1.rb
@@ -1,5 +1,10 @@
 #! /usr/bin/ruby
 
-for i in 100 then
-
+for i in 1..100 then
+  if i % 3 == 0 && i % 5 == 0
+    puts "FizzBuzz"
+  elsif i % 3 == 0
+    puts "Fizz"
+  elsif i % 5 == 0
+  end
 end
